483907e1a0e0e64a47a8d55b485b90ae749ff57b
Tao Gong <tao.gong@uconn.edu>
2016-04-24 09:42:46 -0400

    Finished

--- lu.cpp
+++ lu.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+#include <fstream>
 #include <pthread.h>
+#include <stdexcept>
 #include "pthread_barrier_osx.h"
 using namespace std;
 int N,P,B,NB;
@@ -11,10 +13,9 @@ public:
 	}
 	float& at(int x,int y){
 		if(0<=x&&x<B&&0<=y&&y<B){
-			cerr<<"Block::at Overflow Accessing x="<<x<<",y="<<y<<endl;
-			return p[0];
-		}else{
 			return p[x*B+y];
+		}else{
+			throw range_error("Block::at");
 		}
 	}
 	~Block(){
@@ -29,18 +30,24 @@ public:
 	}
 	Block& getBlock(int x,int y){
 		if(0<=x&&x<NB&&0<=y&&y<NB){
-			cerr<<"getBlock Overflow Accessing x="<<x<<",y="<<y<<endl;
-			return p[0];
-		}else{
 			return p[x*NB+y];
+		}else{
+			throw range_error("Matrix::getBlock");
 		}
 	}
 	float& at(int x,int y){
 		if(0<=x&&x<N&&0<=y&&y<N){
-			cerr<<"Matrix::at Overflow Accessing x="<<x<<",y="<<y<<endl;
-			return p[0].at(0,0);
+			return getBlock(x/B,y/B).at(x%B,y%B);
 		}else{
-			return getBlock(x/NB,y/NB).at(x%NB,y%NB);
+			throw range_error("Matrix::at");
+		}
+	}
+	void print(){
+		for(int i=0;i<N;++i){
+			for(int j=0;j<N;++j){
+				cout<<at(i,j)<<" ";
+			}
+			cout<<endl;
 		}
 	}
 	~Matrix(){
@@ -49,20 +56,68 @@ public:
 }*A;
 pthread_barrier_t barrier;
 void usage(char*n){
-	cout<<"Usage: ./"<<n<<" N P B\n\tN:NxN matrix\n\tP:P threads\n\tB:BxB block"<<endl;
+	cout<<"Usage: ./"<<n<<" N P B matrix_file\n\tN:NxN matrix\n\tP:P threads\n\tB:BxB block"<<endl;
 }
-void top_left(Block&b){//top left block
+
+// block b, referce block r, line "i" in b, line "k" in r, start column j, alpha
+void daxpy(Block&b , Block&r, int i, int k, int j, float alpha)
+{
+	for (int p = j; p<B; p++)     b.at(i,p) += alpha*r.at(k,p);
 }
-void top_right(Block&b,const Block&r){//top right block and top left block
+void top_left(Block&b)
+{
+	float alpha;
+	for (int k=0; k<B; k++) {
+		/* modify subsequent columns */
+		for (int i=k+1; i<B; i++) {
+			b.at(i,k)/= b.at(k,k);
+			alpha = -b.at(i,k);
+			//length = n-k-1;
+			daxpy(b, b, i, k, k+1, alpha);
+		}
+	}
 }
-void bottom_left(Block&b,const Block&r){//bottom left block and top left block
+
+
+void top_right(Block&b, Block&r)
+{
+	float alpha;
+	for (int k=0; k<B; k++) {
+		for (int i=k+1; i<B; i++) {
+			alpha = -r.at(i,k);
+			daxpy(b, b, i, k, 0, alpha);
+		}
+	}
 }
-void bottom_right(Block&b,const Block&r){//bottom right block and bottom left block
+
+
+void bottom_left(Block &b, Block&r)
+{
+	float alpha;
+	for (int k=0; k<B; k++)
+		for (int i=0; i<B; i++) {
+			b.at(i,k) /= r.at(k,k);
+			alpha = -b.at(i,k);
+			daxpy(b, r, i, k, k+1, alpha);
+		}
 }
-void* thread_main(void*p){
+
+
+void bottom_right(Block &b, Block&r1, Block&r2)
+{
+	float alpha;
+	for (int k=0; k<B; k++) {
+		for (int i=0; i<B; i++) {
+			alpha = -r1.at(i,k);
+			daxpy(b,r2,i,k,0,alpha);
+		}
+	}
+}
+
+void* thread_main(void*p)
+{
 	int thread_id=*(int*)p;
 	//pthread_barrier_wait(&barrier);
-	cout<<"thread_id:"<<thread_id<<endl;
 	for(int round=0;round<NB;++round){
 		//step 1, calculate top left
 		if(thread_id==0){
@@ -75,7 +130,7 @@ void* thread_main(void*p){
 		//1 x x
 		//3 x x
 		//0,1,2,3->()
-		for(int i=thread_id;i<=(NB-round-1)*2;i+=P){
+		for(int i=thread_id;i<(NB-round-1)*2;i+=P){
 			if(i&1){
 				//bottom left
 				//A->getBlock(round+(i>>1)+1,round);
@@ -91,22 +146,26 @@ void* thread_main(void*p){
 		//x x x
 		//x 0 1
 		//x 2 3
-		for(int i=thread_id;i<=(NB-round-1)*(NB-round-1);i+=P){
+		for(int i=thread_id;i<(NB-round-1)*(NB-round-1);i+=P){
 			//A->getBlock(i/(NB-round-1)+round+1,i%(NB-round-1)+round+1)
-			bottom_right(A->getBlock(i/(NB-round-1)+round+1,i%(NB-round-1)+round+1),A->getBlock(i/(NB-round-1)+round+1,round));
+			bottom_right(A->getBlock(i/(NB-round-1)+round+1,i%(NB-round-1)+round+1),
+				A->getBlock(i/(NB-round-1)+round+1,round),
+				A->getBlock(round,i%(NB-round-1)+round+1));
 		}
 		pthread_barrier_wait(&barrier);
 	}
 	return NULL;
 }
-int main(int argc, char**argv){
-	if(argc<=3){
+int main(int argc, char**argv)
+{
+	if(argc<=4){
 		usage(argv[0]);
 		return -1;
 	}
 	N=atoi(argv[1]);
 	P=atoi(argv[2]);
 	B=atoi(argv[3]);
+	ifstream fin(argv[4]);
 	if(N==0||P==0||B==0){
 		usage(argv[0]);
 		return -1;
@@ -116,7 +175,7 @@ int main(int argc, char**argv){
 	int i,j;
 	for(i=0;i<N;++i){
 		for(j=0;j<N;++j){
-			cin>>A->at(i,j);
+			fin>>A->at(i,j);
 		}
 	}
 	pthread_barrier_init(&barrier,NULL,P);
@@ -134,12 +193,7 @@ int main(int argc, char**argv){
 	delete[]thread_args;
 	delete[]thread_handle;
 	pthread_barrier_destroy(&barrier);
-	for(i=0;i<N;++i){
-		for(j=0;j<N;++j){
-			cout<<A->at(i,j)<<" ";
-		}
-		cout<<endl;
-	}
+	A->print();
 	
-	
+	return 0;
 }
